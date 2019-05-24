137619e725535ee5398ec0b533507a56582ab408
vsilent <jabberroid@gmail.com>
2019-05-24 13:05:13 +0300

    env var fixed, php-fpm test update

--- tests.py
+++ tests.py
@@ -21,8 +21,8 @@ print(php_conf.output.decode())
 assert 'configuration file /etc/php/7.1/fpm/php-fpm.conf test is successful' in php_conf.output.decode()
 php_proc = php.exec_run("sh -c 'ps aux|grep php-fpm'")
 print(php_proc.output.decode())
-# assert 'php-fpm: master process (/usr/local/etc/php-fpm.conf)' in php_proc.output.decode()
-assert 'fpm is running, pid' in php_proc.output.decode()
+assert 'php-fpm: master process (/etc/php/7.1/fpm/php-fpm.conf)' in php_proc.output.decode()
+assert 'php-fpm: pool www' in php_proc.output.decode()
 
 mysql = client.containers.get('pimcoredb')
 assert mysql.status == 'running'
@@ -31,7 +31,8 @@ mycnf = mysql.exec_run("/usr/sbin/mysqld --verbose --help")
 assert 'mysqld  Ver 5.7' in mycnf.output.decode()
 mysql_log = mysql.logs()
 assert "mysqld: ready for connections" in mysql_log.decode()
-# print(mysql_log.decode())
+print(mysql_log.decode())
 
-# response = requests.get("http://localhost")
+response = requests.get("http://localhost")
+print(response)
 # assert response.status_code == 200
