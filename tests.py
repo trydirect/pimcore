cacab66ba4792cbfe694e4165c7571df7c3874d8
vsilent <jabberroid@gmail.com>
2020-01-27 16:00:26 +0200

    test update for php 7.3

--- tests.py
+++ tests.py
@@ -16,12 +16,12 @@ for c in client.containers.list():
 # PHP
 php = client.containers.get('pimcore')
 assert php.status == 'running'
-php_conf = php.exec_run("php-fpm7.1 -t")
+php_conf = php.exec_run("php-fpm7.3 -t")
 print(php_conf.output.decode())
-assert 'configuration file /etc/php/7.1/fpm/php-fpm.conf test is successful' in php_conf.output.decode()
+assert 'configuration file /etc/php/7.3/fpm/php-fpm.conf test is successful' in php_conf.output.decode()
 php_proc = php.exec_run("sh -c 'ps aux|grep php-fpm'")
 print(php_proc.output.decode())
-assert 'php-fpm: master process (/etc/php/7.1/fpm/php-fpm.conf)' in php_proc.output.decode()
+assert 'php-fpm: master process (/etc/php/7.3/fpm/php-fpm.conf)' in php_proc.output.decode()
 assert 'php-fpm: pool www' in php_proc.output.decode()
 
 mysql = client.containers.get('pimcoredb')
